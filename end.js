2e51bb01c901c01a8bb716a3d8f01d2445d1e1c5
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-08-11 21:38:28 -0600

    no errors

--- end.js
+++ end.js
@@ -2,8 +2,8 @@ var username = document.getElementById("username");
 var saveScoreBtn = document.getElementById("saveScoreBtn");
 var finalScore = document.getElementById("finalScore");
 var mostRecentScore = localStorage.getItem("mostRecentScore");
-var highScoresList = document.getElementById("highScoresList");
-var highScores = document.getElementById = ("highScores");
+
+
 finalScore.innerText = mostRecentScore;
 
 //gives array of high scores
@@ -13,7 +13,7 @@ var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value
 });
-z
+
 saveHighScore = e => {
   e.preventDefault();
 
@@ -22,6 +22,7 @@ var score = {
   mostRecentScore,
   name: username.value
 };
+
 highScores.push(score);
 console.log(highScores);
 
